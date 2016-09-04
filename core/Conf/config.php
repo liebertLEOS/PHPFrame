0bddc891aeac841c4a6f4ac6a8a4248a96c66cb8
libert <845301110@qq.com>
2016-09-04 14:00:25 +0800

    Create config.php

---
+++ core/Conf/config.php
@@ -0,0 +1,30 @@
+<?php
+return array(
+    'DB_TYPE'                 => '',
+    'DB_USER'                 => '',
+    'DB_PWD'                  => '',
+    'DB_HOST'                 => '',
+    'DB_PORT'                 => '',
+    'DB_NAME'                 => '',
+    'DB_DSN'                  => '',
+    'DB_PARAMS'               => '', 
+    'DB_PREFIX'               => '',  
+    'DB_CHARSET'              => 'utf8',
+    'DEFAULT_M_LAYER'         => 'Model', // 默认的模型层名称
+    'DEFAULT_C_LAYER'         => 'Controller', // 默认的控制器层名称
+    'DEFAULT_V_LAYER'         => 'View', // 默认的视图层名称
+    'DEFAULT_B_LAYER'         => 'Behavior', // 默认的行为层名称
+    'DEFAULT_MODULE'          => 'Home',
+    'DEFAULT_CONTROLLER'      => 'Index',
+    'DEFAULT_FUNCTION'        => 'index',
+    'DEFAULT_CHARSET'         => 'utf-8', // 默认输出编码
+    'TMPL_CONTENT_TYPE'       => 'text/html', // 默认模板输出类型
+    'TMPL_TEMPLATE_SUFFIX'    => 'tpl',//模板后缀
+    'TMPL_COMPILER_SUFFIX'    => 'html.php',
+    'HTTP_CACHE_CONTROL'      => 'private', // 网页缓存控制
+    'TMPL_ENGINE_TYPE'        => 'Template',  //引擎类型定义
+    'TAGLIB_BEGIN'            => '<',
+    'TAGLIB_END'              => '>',
+    'DEFAULT_AJAX_DATA_TYPE'  => 'json'  //ajax返回数据类型
+);
+?>
